ea3a738136e075468183cdd2f6cee85f998e114c
ALvaniseo <116092256+ALvaniseo@users.noreply.github.com>
2024-01-18 12:53:45 +0100

    update entity's and add exceptionSubscriber to catch errors

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,11 +6,12 @@ use App\Entity\Customer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class CustomerController extends AbstractController
@@ -66,8 +67,12 @@ class CustomerController extends AbstractController
 
         // Normaliser l'entité Customer en JSON
         $jsonCustomer = $serializer->serialize($customerSerialize, 'json');
-
         
-        return new JsonResponse($jsonCustomer, JsonResponse::HTTP_CREATED);
+        
+        return new JsonResponse($jsonCustomer,
+            Response::HTTP_CREATED,
+            [],
+            true
+        );
     }
 }
